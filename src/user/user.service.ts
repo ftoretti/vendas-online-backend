38ddab67e684744d6452b8318f2add1fe84884d7
FERNANDO TORETTI <fernandotorettidemelo@gmail.com>
2023-07-31 22:59:21 -0300

    feat: insert validation in address

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm'
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './entities/user.entity';
@@ -26,5 +26,20 @@ export class UserService {
 
     async getAllUser(): Promise<UserEntity[]> {
         return this.userRepository.find();
+    }
+
+    async findUserById(userId: number): Promise<UserEntity> {
+        const user = await this.userRepository.findOne({
+            where: {
+                id: userId
+            }
+        });
+
+        if (!user) {
+            throw new NotFoundException(`UserId: ${userId} not found.`);    
+
+        }
+
+        return user;
     }
 }
